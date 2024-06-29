a729122cb9aa0f88605aa3ea5fdc248e94fd4748
MrTomMus <tommusshow@gmail.com>
2024-06-29 14:16:48 +0300

    feat: проект добавлен в репозиторий

---
+++ burgerMenu.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.header-burgerContainer');
+const menu = document.querySelector('.header-burgerMenu');
+const btnBurgerMenu = document.querySelector('.header__burgerMenu');
+
+
+
+
+button.addEventListener('click', () => {
+    console.log('test')
+    menu.classList.toggle('header-burgerMenu--active');
+    btnBurgerMenu.classList.toggle('header__burgerMenu--active')
+})
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,18 @@
+const btnPopup = document.querySelectorAll('.buttonPopUp');
+const popUpcontainer = document.querySelector('.popup-container');
+const popup = document.querySelector('.popup-container__popup');
+
+console.log(btnPopup)
+
+
+btnPopup.forEach(elem => elem.addEventListener('click', () => {
+    popUpcontainer.classList.toggle('popup-container--active')
+}))
+
+popUpcontainer.addEventListener('click', (e) => {
+    let elem = e.target;
+
+    if(!elem.classList.contains('popup-container__popup')) {
+        popUpcontainer.classList.remove('popup-container--active')
+    }
+})
